c77793d967cd97553a625959b802c6c2a51bfb98
jhony0413 <jhonysilva29082000@gmail.com>
2026-05-11 20:26:35 -0300

    Add test cases for ProvaUCB and ProvaFafifo

--- universidade/prova/teste/Teste.java
+++ universidade/prova/teste/Teste.java
@@ -4,6 +4,23 @@ import universidade.prova.*;
 
 public class Teste {
     public static void main(String[] args) {
-        
+        System.out.println("----------UCB----------");
+        ProvaUCB ucb = new ProvaUCB(6, 7);
+        System.out.println("Média inicial: " + ucb.calcularMedia());
+        System.out.println("Aprovado: " + ucb.aprovado());
+        ucb.getProvaFinalUniversidade(8);
+        System.out.println("Média após AV3: " + ucb.calcularMedia());
+        System.out.println("Aprovado: " + ucb.aprovado());
+
+        System.out.println("--------FAFIFO---------");
+        ProvaFafifo fafifo = new ProvaFafifo(7, 9);
+        System.out.println("Média inicial: " + fafifo.calcularMedia());
+        System.out.println("Aprovado: " + fafifo.aprovado());
+
+        if (!fafifo.aprovado()) {
+            fafifo.getProvaFinalUniversidade(7);
+            System.out.println("Média após final: " + fafifo.calcularMedia());
+            System.out.println("Aprovado: " + fafifo.aprovado());
+        }
     }
 }
